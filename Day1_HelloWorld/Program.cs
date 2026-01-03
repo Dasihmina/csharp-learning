0db2c3d7ef177afac9729f4b0077a18cbcae155f
dasih <dasih@DIANA>
2026-01-03 23:30:51 +0200

    Add project files.

---
+++ Day1_HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main() {
+
+        Console.Write("How old are you?: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"You are {age} years old!");
+
+        if (age >= 18) {
+            Console.WriteLine("You are old enough to vote!");
+        } else { 
+            Console.WriteLine("You aren`t old enough to vote!");
+        }
+
+     }
+}
