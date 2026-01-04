82f41f743cdd73098d63c658408699ebda4d1281
dasih <dasihmina@inbox.lv>
2026-01-04 12:20:32 +0200

    Day 2 - variables and input

---
+++ Day2_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("What is your favourite color?: ");
+        string color = Console.ReadLine();
+
+        Console.Write("What is your favourite number?: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Your favourite color is {color} and favourite number is {number}.");
+        Console.ReadLine();
+    }
+
+
+}
