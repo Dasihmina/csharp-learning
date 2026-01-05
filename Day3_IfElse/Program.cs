32c3cd76dd2a29a960807b95f8c4388414b40ad9
Dasihmina <136841017+Dasihmina@users.noreply.github.com>
2026-01-05 17:13:26 +0200

    Add files via upload

---
+++ Day3_IfElse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Choose a number: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Number is even");
+        }
+        else
+        {
+            Console.WriteLine("Number is odd");
+        }
+    }
+}
