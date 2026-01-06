5e9f8f60ca05dbb635a9ab6afcacb6a73a1b42dd
Dasihmina <136841017+Dasihmina@users.noreply.github.com>
2026-01-06 23:05:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Choose a number (1-3): ");
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Hello");
+                break;
+
+            case 2:
+                Console.WriteLine("Bye");
+                break;
+
+            case 3:
+                Console.WriteLine("Exit");
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
