736bf74b538853d5a24371137760cb35e982d67e
Dasihmina <136841017+Dasihmina@users.noreply.github.com>
2026-01-06 23:10:44 +0200

    Add files via upload

---
+++ Day4_loopsAndSumProgram2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) 
+        {
+            Console.WriteLine(i);
+            sum += i;
+            ++i;
+
+           
+        }
+        Console.WriteLine("Sum = " + sum);
+        Console.ReadLine();
+     }
+}
